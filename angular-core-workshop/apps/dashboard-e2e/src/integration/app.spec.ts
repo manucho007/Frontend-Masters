afa191fe1b2155280242150c67e5e9356527ddde
Manuel Rodriguez <manrodri92@gmail.com>
2019-12-04 02:47:03 +0300

    Changed: renamed angular project to dashboard

--- angular-core-workshop/apps/dashboard-e2e/src/integration/app.spec.ts
+++ angular-core-workshop/apps/dashboard-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('angular-core', () => {
+describe('dashboard', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('angular-core', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to angular-core!');
+    getGreeting().contains('Welcome to dashboard!');
   });
 });
--- angular-core-workshop/apps/dashboard/jest.config.js
+++ angular-core-workshop/apps/dashboard/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'angular-core',
+  name: 'dashboard',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/angular-core',
+  coverageDirectory: '../../coverage/apps/dashboard',
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js'
